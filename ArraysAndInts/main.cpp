7f6a50c67a70f5615141b628c2c63f9cd0b7abb0
Noah <nvansickle18@kentdenver.org>
2018-05-01 08:54:48 -0600

    arraysandints

--- ArraysAndInts/main.cpp
+++ ArraysAndInts/main.cpp
@@ -7,9 +7,30 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+string* fruit[3] = {}; //getting errors, you said this would happen!!!
+string colour[4] = {"Blue", "Red", "Orange", "Yellow"}; //array of strings
+int array1 [5] = { 16, 2, 77, 40, 12071 }; //Array of Ints
+int n, result=0;
+int main(int argc, const char * argv[])
+    {
+        for ( n=0 ; n<5 ; ++n )
+        {
+            result += array1[n];
+        }
+        cout << result;
+        
+        // Print Strings
+        for (int i = 0; i < 4; i++)
+            cout << colour[i] << "\n";
+        
+        
+        return 0;
+        
+    }
+
+    
+    
+
+
