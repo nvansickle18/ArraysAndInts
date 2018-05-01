40d90087e6ae253f797813764d5a441c7d7333c2
Noah <nvansickle18@kentdenver.org>
2018-05-01 08:44:35 -0600

    Initial Commit

---
+++ ArraysAndInts/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArraysAndInts
+//
+//  Created by Noah VanSickle on 5/1/18.
+//  Copyright © 2018 Noah VanSickle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
